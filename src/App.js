e9f4b1a0d3a540e5634fcbbc1458c2d10c6e0508
Krishil Rana <krishilrana22@gmail.com>
2024-10-20 21:55:08 +0530

    Resolved Merge confilcts.

--- src/App.js
+++ src/App.js
@@ -9,14 +9,11 @@ import Hero from './Components/Hero/Hero';
 import Testimonial from './Components/Testimonial/Testimonial';
 import testimonials from './Components/Testimonial/testimonialsData';
 import Scroll from './Components/ScrollToTop/Scroll';
-<<<<<<< Updated upstream
-=======
 import Query from './Components/Query/Query';
 
 // import Testimonial from "./Components/Testimonial/Testimonial";
 // import testimonialsData from "./Components/Testimonial/testimonialsData";
 // import Card from "./Components/Testimonial/Card";
->>>>>>> Stashed changes
 
 function App() {
   return (
